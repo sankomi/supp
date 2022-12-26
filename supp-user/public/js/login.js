a17acc85e9c2532f9fbb7413493ab41d6fa3f913
sanko <sang.koh@hotmail.com>
2022-12-26 17:40:17 +1100

    pretty?

--- supp-user/public/js/login.js
+++ supp-user/public/js/login.js
@@ -1,5 +1,6 @@
 (async () => {
 	window.addEventListener("show", showApp);
+	
 	function showApp(event) {
 		window.removeEventListener("show", showApp);
 		
@@ -19,14 +20,38 @@
 	);
 	let json = await res.json();
 	if (json.username) {
+		const hello = document.querySelector("#hello");
+		if (hello) hello.textContent = "hello, " + json.username + "!";
+		document.querySelector("#login")?.remove();
 		window.dispatchEvent(new CustomEvent("login", {detail: {
 			login: true,
 			username: json.username,
 			support: json.support,
 		}}));
 	} else {
+		document.querySelector("#logout")?.remove();
+		document.querySelector("#home")?.remove();
 		window.dispatchEvent(new CustomEvent("login", {detail: {
 			login: false,
 		}}));
 	}
+	if (!json.support) document.querySelector("#users").remove();
+	
+	const logout = document.querySelector("#logout");
+	if (logout) logout.addEventListener("click", async event => {
+		event.preventDefault();
+		let res = await fetch(
+			"http://localhost:3000/user/logout/",
+			{
+				credentials: "include",
+				method: "DELETE",
+				headers: {
+					"Accept": "application/json",
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		let json = await res.json();
+		if (json.result === "success") window.location.href = "/login/";
+	});
 })();
